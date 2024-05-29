d2d716aeb9daebb31bc46b3f52bb2c57e4df2fbf
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-05-29 14:09:07 -0500

    Adding the logo.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,22 @@
+import Image from "next/image";
+
 export const Header = () => {
   return (
-    <div className="max-w-[500px] mx-auto mb-6">
-      <h1 className="text-heading text-white font-semibold text-center mb-2">
+    <div className="max-w-[500px] mx-auto mb-6 relative">
+      <h1 className="text-heading text-white font-semibold text-center mb-2 relative z-10">
         Our Collection
       </h1>
-      <p className="text-base text-gray font-semibold text-center leading-tight">
+      <p className="text-base text-gray font-semibold text-center leading-tight relative z-10">
         Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.
       </p>
+      <Image
+        src={'/vector.svg'}
+        alt="Icon"
+        width={257}
+        height={153}
+        priority
+        className="absolute -top-14 -right-2"
+      />
     </div>
   );
 };
